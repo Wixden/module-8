9d39726a2fc782930698ed9c79b61df7427c5c60
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-11-19 00:47:56 +0600

    setup environment

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import app from "./app";
+import config from "./app/config";
+import mongoose from "mongoose";
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_URL as string);
+
+    app.listen(config.port, () => {
+      console.log(
+        `Example app listening on port http://localhost:${config.port}`
+      );
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+main();
