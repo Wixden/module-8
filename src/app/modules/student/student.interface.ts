dd4a9bb7e0844284687931ed125286f9193c9e3d
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-11-19 05:48:09 +0600

    Student Interface created

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,43 @@
+export interface Address {
+  street: string;
+  city: string;
+  postalCode: string;
+}
+
+export interface Guardian {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherPhone: string;
+  motherName: string;
+  motherOccupation: string;
+  motherPhone: string;
+}
+
+export interface LocalGuardian {
+  name: string;
+  email: string;
+  phone: string;
+  occupation: string;
+  address: Address;
+}
+
+export interface Student {
+  id: string;
+  name: {
+    firstName: string;
+    middleName: string;
+    lastName: string;
+  };
+  gender: 'Male' | 'Female';
+  birthDate: string;
+  email: string;
+  phoneNumber: string;
+  emergencyPhoneNumber: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: Address;
+  permanentAddress: Address;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: 'Active' | 'InActive';
+}
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,16 @@
-import app from "./app";
-import config from "./app/config";
-import mongoose from "mongoose";
+import app from './app';
+import config from './app/config';
+import mongoose from 'mongoose';
 
 async function main() {
   try {
     await mongoose.connect(config.database_URL as string);
 
     app.listen(config.port, () => {
-      console.log(
-        `Example app listening on port http://localhost:${config.port}`
-      );
+      console.log(`App is listening on port http://localhost:${config.port}`);
     });
   } catch (err) {
-    console.log(err.message);
+    console.log(err);
   }
 }
 
