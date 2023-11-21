f1f7a176a4b3a6fd2357adfe13d9ec145bbf3b02
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-11-21 22:00:49 +0600

    Joi field validation

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { StudentServices } from './student.service';
+import Joi from 'joi';
 
 const createStudent = async (req: Request, res: Response) => {
   try {
